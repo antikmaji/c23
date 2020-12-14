a47c23e5982ea384ab2b4595785a9e70789f906a
antikmaji <74954769+antikmaji@users.noreply.github.com>
2020-12-14 13:39:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1= new Box(150,150,50,50);
+    box2=new Box(190,75,50,100);
+    ground=new Ground(200,height,400,25);
+
+ 
+}
+
+//box1 = new Box();
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    
+  box1.display();
+  box2.display();
+ground.display();
+
+}
+
+//box1.width
+//box
